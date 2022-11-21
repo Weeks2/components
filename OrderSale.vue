873cd438671f2c205f6ef3c916d4fc439e0ed287
erika-patricia-git <08.erika.gon@gmail.com>
2022-11-21 13:24:59 -0600

    Create OrderSale.vue

---
+++ OrderSale.vue
@@ -0,0 +1,10 @@
+<template>
+    
+    <label for="">Cliente</label>
+    <select name="" id="" class="form-control">
+        <option value="">Cliente 1</option>
+        <option value="">Cliente 2</option>
+    </select>
+
+
+</template>
